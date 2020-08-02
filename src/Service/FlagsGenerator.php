1be62eafc7f0cb09229af26ec26db58e71821794
Artem <artem.kyiv@outlook.com>
2020-08-02 13:20:53 +0300

    refactored

--- src/Service/FlagsGenerator.php
+++ src/Service/FlagsGenerator.php
@@ -4,18 +4,14 @@ namespace Rteeom\EmojiISOFlagsBundle\Service;
 
 use Rteeom\Exceptions\IsoFlagGeneratorException;
 use Rteeom\FlagsGenerator as BaseFlagGenerator;
-use Rteeom\CountryCodeValidator;
 
 class FlagsGenerator
 {
     private BaseFlagGenerator $flagService;
 
-    private CountryCodeValidator $validator;
-
     public function __construct()
     {
         $this->flagService = new BaseFlagGenerator();
-        $this->validator = new CountryCodeValidator();
     }
 
     /**
@@ -25,22 +21,11 @@ class FlagsGenerator
      */
     public function getEmojiFlag(string $isoCountryCode): string
     {
-        if (null === $flag = $this->flagService->getEmojiFlagOrNull($isoCountryCode)) {
-            throw new IsoFlagGeneratorException();
-        }
-
-        return $flag;
+        return $this->flagService->getEmojiFlag($isoCountryCode);
     }
 
     public function getEmojiFlagOrNull(string $isoCountryCode): ?string
     {
-        if ($this->validator->isValid($isoCountryCode)) {
-            $first = dechex(ord($isoCountryCode[0])+127365);
-            $second = dechex(ord($isoCountryCode[1])+127365);
-
-            return mb_convert_encoding("&#x$first;"."&#x$second;","UTF-8","HTML-ENTITIES");
-        }
-
-        return null;
+        return $this->flagService->getEmojiFlagOrNull($isoCountryCode);
     }
 }
